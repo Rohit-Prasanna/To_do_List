586e40212ca0a15fd66301140fc88b4fea803057
Rohit-Prasanna <115570423+Rohit-Prasanna@users.noreply.github.com>
2023-05-18 12:14:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,101 +1,120 @@
-const addTodo = function(todo) {
-    const isHidden = todo.dropdown ? "" : "hidden";
-    const isChecked = todo.checked ? "checked" : "";
-    const dropdownIcon = todo.dropdown ? "fa-solid fa-circle-chevron-up" : "fa-solid fa-circle-chevron-down";
-    const rawhtml = `
-      <div id="item-ct" >
+const todoList = document.getElementById("list");
+const remainingText = document.getElementById("remaining-task");
+const addButton = document.getElementById("add-btn");
+
+let todos = [];
+
+function updateUI() {
+  todoList.innerHTML = "";
+  remainingText.textContent = todos.filter(todo => !todo.checked).length;
+
+  todos.forEach(todo => {
+    const li = document.createElement("li");
+    li.classList.toggle("checked", todo.checked);
+    li.innerHTML = `
+      <div id="item-ct">
         <div class="topic" id="topic-ct">
           <span>${todo.title}</span>
-          <i class="${dropdownIcon}" type="button" onclick ="toggleDropdown(\'${todo.title}\')"></i>
+          <i class="fa-solid ${todo.dropdown ? "fa-circle-chevron-up" : "fa-circle-chevron-down"}" type="button" onclick="toggleDropdown('${todo.id}')"></i>
+        
+      </div>
         </div>
-        <div id="dropdown-ct" ${isHidden}>
+        <div id="dropdown-ct-${todo.id}" ${todo.dropdown ? "" : "hidden"}>
           <div class="drpbtn"> 
             <span>${todo.date}</span>
             <span>${todo.time}</span>
             <div>
-              <i class="fa-solid fa-check-circle"  onclick = "checkItem(\'${todo.title}\')"s></i>
-              <i class="fa-solid fa-trash" onclick = "deleteItem(\'${todo.title}\')"></i>
+            <i class="fa-solid fa-edit" type="button" onclick="editItem('${todo.id}')"></i>
+              <i class="fa-solid fa-check-circle" onclick="checkItem('${todo.id}')"></i>
+              <i class="fa-solid fa-trash" onclick="deleteItem('${todo.id}')"></i>
             </div>
           </div>
         </div>  
       </div>
     `;
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
-    li.className = isChecked;
-    li.innerHTML = rawhtml;
-    ul.appendChild(li);
+    todoList.appendChild(li);
+  });
+
+  // Store todos in localStorage
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function toggleDropdown(id) {
+  todos = todos.map(todo => {
+    if (todo.id === id) {
+      return { ...todo, dropdown: !todo.dropdown };
+    }
+    return todo;
+  });
+  updateUI();
+}
+
+function deleteItem(id) {
+  todos = todos.filter(todo => todo.id !== id);
+  updateUI();
+}
+
+function checkItem(id) {
+  todos = todos.map(todo => {
+    if (todo.id === id) {
+      return { ...todo, checked: true };
+    }
+    return todo;
+  });
+  updateUI();
+}
+
+function onAddButtonClicked() {
+  const titleInput = document.getElementById("topic");
+  const dateInput = document.getElementById("date");
+  const timeInput = document.getElementById("time");
+
+  const newTodo = {
+    id: Date.now().toString(),
+    title: titleInput.value,date: dateInput.value,time: timeInput.value,
+    dropdown: false, checked: false
   };
-  
-  const generateTodo = function(todos) {
-    const list = document.getElementById("list");
-    list.innerHTML = "";
-    remainingText.innerHTML = todos.filter(todo => !todo.checked).length;
-    todos.forEach(todo => {
-      addTodo(todo);
-    });
-    
-    // Store todos in localStorage
-    localStorage.setItem("todos", JSON.stringify(todos));
-  };
-  
-  var remainingTask = 0;
-  var todos = [];
-  const remainingText = document.getElementById("remaining-task");
-  
-  const toggleDropdown = function(title) {
-    todos = todos.map(item => {
-      if (item.title === title) {
-        return {...item, dropdown: !item.dropdown};
-      }
-      return item;
-    });
-    generateTodo(todos);
-  };
-  
-  const deleteItem = function(title) {
-    todos = todos.filter(value => value.title != title);
-    generateTodo(todos);
-  };
-  
-  const checkItem = function(title) {
-    todos = todos.map(item => {
-      if (item.title === title) {
-        return {...item, checked: true};
-      }
-      return item;
-    });
-    generateTodo(todos);
-  };
-  
-  const onAddbtnclicked = function() {
-    const titletxt = document.getElementById("topic");
-    const datetxt = document.getElementById("date");
-    const timetxt = document.getElementById("time");
-    todos.push({title: titletxt.value, date: datetxt.value, time: timetxt.value, dropdown: false, checked: false});
-    generateTodo(todos);
-  };
-  
-  const clearAll = function() {
-    const list = document.getElementById("list");
-    list.innerHTML = "";
-    todos = [];
-    remainingText.innerHTML = 0;
-    
-    // Remove todos from localStorage
-    localStorage.removeItem("todos");
+
+  todos.push(newTodo);
+  updateUI();
+
+  // Clear inputs given prior
+  titleInput.value = "";
+  dateInput.value = "";
+  timeInput.value = "";
+}
+
+function clearAll() {
+  todos = [];
+  updateUI();
+
+
+
+    // // Remove todos from localStorage
+    // localStorage.removeItem("todos");//not needed
   };
   
   // Retrieve todos from localStorage if they exist
   const storedTodos = localStorage.getItem("todos");
   if (storedTodos) {
     todos = JSON.parse(storedTodos);
-    generateTodo(todos);
+    updateUI();
   }
   
-  const addButton = document.getElementById("add-btn");
-  addButton.addEventListener("click", onAddbtnclicked);
-  
+
+addButton.addEventListener("click", onAddButtonClicked);
 
 
-    
+  function editItem(id) {
+  const todo = todos.find(todo => todo.id === id);
+  const newTitle = prompt("Enter new title", todo.title);
+  const newDate = prompt("Enter new date", todo.date);
+  const newTime = prompt("Enter new time", todo.time);
+
+  if (newTitle !== null) {
+    todo.title = newTitle;
+    todo.date = newDate;
+    todo.time = newTime;
+    updateUI();
+  }
+}
